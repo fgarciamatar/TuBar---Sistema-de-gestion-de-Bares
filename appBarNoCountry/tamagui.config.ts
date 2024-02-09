080d44d87a3c43d5dfd9743bf43ba33fd212a7d4
Edson Aju <avataredson@gmail.com>
2024-02-08 20:57:47 -0600

    [ADD] FIRST COMMIT

---
+++ appBarNoCountry/tamagui.config.ts
@@ -0,0 +1,78 @@
+// @tamagui/core doesn't include `createMedia` so that it can avoid
+// a dependency on react-native. If you are using tamagui, you can
+// import createMedia from there directly and avoid this line:
+import {createAnimations} from '@tamagui/animations-react-native';
+import {createInterFont} from '@tamagui/font-inter';
+import {shorthands} from '@tamagui/shorthands';
+import {themes, tokens} from '@tamagui/themes';
+
+import {createMedia} from '@tamagui/react-native-media-driver';
+import {createTamagui} from 'tamagui';
+
+const animations = createAnimations({
+  bouncy: {
+    type: 'spring',
+    damping: 10,
+    mass: 0.9,
+    stiffness: 100,
+  },
+  lazy: {
+    type: 'spring',
+    damping: 20,
+    stiffness: 60,
+  },
+  quick: {
+    type: 'spring',
+    damping: 20,
+    mass: 1.2,
+    stiffness: 250,
+  },
+});
+
+const headingFont = createInterFont();
+const bodyFont = createInterFont();
+
+const config = createTamagui({
+  animations,
+  defaultTheme: 'dark',
+  shouldAddPrefersColorThemes: false,
+  themeClassNameOnRoot: false,
+  shorthands,
+  fonts: {
+    heading: headingFont,
+    body: bodyFont,
+  },
+  themes,
+  tokens,
+  media: createMedia({
+    xs: {maxWidth: 660},
+    sm: {maxWidth: 800},
+    md: {maxWidth: 1020},
+    lg: {maxWidth: 1280},
+    xl: {maxWidth: 1420},
+    xxl: {maxWidth: 1600},
+    gtXs: {minWidth: 660 + 1},
+    gtSm: {minWidth: 800 + 1},
+    gtMd: {minWidth: 1020 + 1},
+    gtLg: {minWidth: 1280 + 1},
+    short: {maxHeight: 820},
+    tall: {minHeight: 820},
+    hoverNone: {hover: 'none'},
+    pointerCoarse: {pointer: 'coarse'},
+  }),
+});
+
+type AppConfig = typeof config;
+
+// this will give you types for your components
+// note - if using your own design system, put the package name here instead of tamagui
+declare module 'tamagui' {
+  interface TamaguiCustomConfig extends AppConfig {}
+
+  // if you want types for group styling props, define them like so:
+  interface TypeOverride {
+    groupNames(): 'a' | 'b' | 'c';
+  }
+}
+
+export default config;
