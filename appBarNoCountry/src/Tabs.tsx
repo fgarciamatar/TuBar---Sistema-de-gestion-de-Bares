360f1a80a1455d1e303225f0ae1e65255d62dc26
fgarciamatar <fgarciamatar@gmail.com>
2024-02-17 14:52:16 -0300

    na

--- appBarNoCountry/src/Tabs.tsx
+++ appBarNoCountry/src/Tabs.tsx
@@ -1,14 +1,10 @@
 import * as React from 'react';
 import {createStackNavigator} from '@react-navigation/stack';
-<<<<<<< HEAD
+
 import Login from './components/Login/Login';
 import Welcom from './components/Welcom/Welcom';
 import SignUp from './components/SignUp/SignUp';
-=======
-import Login from './components/landingPage/Login/Login.tsx';
-import Welcom from './components/landingPage/Welcom/Welcom.tsx';
-import SignUp from './components/landingPage/SignUp/SignUp.tsx';
->>>>>>> a618f134121e920299ba62aadb9f8e78d44b6f1d
+
 
 const Stack = createStackNavigator();
 
