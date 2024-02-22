61e8afa38a30cc346742065747a1b6afb7e91b01
fgarciamatar <fgarciamatar@gmail.com>
2024-02-22 16:46:26 -0300

    cambbbios

--- appBarNoCountry/src/components/Tables/Tables.jsx
+++ appBarNoCountry/src/components/Tables/Tables.jsx
@@ -13,7 +13,7 @@ function Tables() {
   const mesas = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11];
 
   const handleSend = () => {
-    navigation.navigate('Comanda');
+    navigation.navigate('Order');
   };
 
   return (
